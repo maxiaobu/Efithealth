1d1fa5dc36bb06a1d94e7e01e4ed826bf0cedae1
maxiaobu <maxiaobu1999@163.com>
2016-08-20 13:39:11 +0800

    Initial commit

---
+++ app/src/main/java/com/efithealth/app/activity/LunchDetailActivity.java
@@ -0,0 +1,17 @@
+package com.efithealth.app.activity;
+
+import android.os.Bundle;
+
+import com.efithealth.R;
+import com.efithealth.app.maxiaobu.base.BaseAty;
+
+public class LunchDetailActivity extends BaseAty {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lunch_detail);
+
+
+    }
+}
